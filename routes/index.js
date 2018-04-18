a6e60d76201bd1683ebe424b1345ad71660c95c6
manu35 <manu05091991@gmail.com>
2018-04-18 15:34:18 -0500

    Manage submit book form on front end.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+router.post('/', function(req, res, next) {
+  console.log(`Name: ${req.body.name}`);
+  console.log(`Email: ${req.body.email}`);
+  let date = new Date(req.body.datePicker);
+  console.log(`Date: ${date.toDateString()}`);
+  console.log(`Persons: ${Number.parseInt(req.body.people)}`);
+  console.log(`Comments: ${req.body.coments}`);
+  console.log(`Trip name: ${req.body.tripName}`);
+  console.log(`Trip ID: ${req.body.tripId}`);
+  console.log(`Trip price: ${Number.parseInt(req.body.tripPrice)}`);
+  console.log(`Final price: ${Number.parseInt(req.body.finalPrice)}`);
+  if (process.env.PROD !== 'true') {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  }
+  res.sendStatus(200);
 });
 
 module.exports = router;
