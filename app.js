a925c9186ff4e18b538781be7f643d0ac3f39ffe
manu35 <manu05091991@gmail.com>
2019-03-24 19:08:52 -0300

    Adding cors

--- app.js
+++ app.js
@@ -9,22 +9,19 @@ var email = require('./routes/email')
 
 var app = express();
 
-// make Express use our redirect middleware
-// if (process.env.PROD === 'true') {
-//   app.use(function(req, res, next) {
-//     if (req.hostname !== process.env.PROD_URL) {
-//       return res.redirect(`https://${process.env.PROD_URL}/${req.originalUrl}`);
-//     }
-//     return next(); // call the next middleware (or route)
-//   });
-// }else {
-//   app.use(function(req, res, next){
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     next();
-//   })
-// }
+if (process.env.PROD === 'true') {
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://sunzuntour.now.sh');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  })
+} else {
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  })
+}
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,14 +30,14 @@ app.use('/', index);
 app.use('/email', email)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
